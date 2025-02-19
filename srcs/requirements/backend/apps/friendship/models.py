cd8ce4ba30c6848847525e77559783d258cd2347
Thomas Lam <thomas.lam0411@gmail.com>
2025-02-19 16:41:46 +0100

    fixes + added display other profile

--- srcs/requirements/backend/apps/friendship/models.py
+++ srcs/requirements/backend/apps/friendship/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import UserProfile
 
-# Create your models here.
+class Friendship(models.Model):
+    status =    models.CharField(
+        max_length=11,
+        choices=[("not_friends", "Not Friends"), ("pending", "Pending"), ("accepted", "Accepted"),]
+        default="not_friends",
+        )
+    friend1 =   models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="friend of")
+    friend2 =   models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="friend of")
