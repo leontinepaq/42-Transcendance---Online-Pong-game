41aa49b5fb1c6ade49d31c5ac7e2b40cdf25092f
leontinepaq <leontine.paquatte@gmail.com>
2025-02-19 01:57:01 +0100

    clean 2fa + rm modals ds html

--- srcs/requirements/frontend/site/js/eventDelegator.js
+++ srcs/requirements/frontend/site/js/eventDelegator.js
@@ -5,6 +5,7 @@ import { signupActions } from "./actions/signup.js";
 import { pongActions } from "./actions/pong.js";
 import { friendsActions } from "./actions/friends.js";
 import { profileActions } from "./actions/profile.js";
+import { verify2faEmailActions } from "./actions/validate2faEmail.js"
 // import { TwofaActions } from "./actions/2fa.js";
 
 // Table des actions à gérer, "..." = concat
@@ -15,7 +16,8 @@ const clickActions = [
 	...signupActions,
 	...pongActions,
 	...friendsActions,
-	...profileActions
+	...profileActions,
+	...verify2faEmailActions
 	// ...TwofaActions,
 ];
 
--- srcs/requirements/frontend/site/js/routes.js
+++ srcs/requirements/frontend/site/js/routes.js
@@ -1,9 +1,11 @@
 import { loadUserProfile } from "./actions/profile.js";
+import { send2faEmail } from "./actions/validate2faEmail.js";
 // import { loadDashboardStats } from "./actions/dashboard.js";
 
 // Table de routage : associe chaque route à sa fonction d'initialisation
 const onRouteLoad = {
-    profile: loadUserProfile
+    profile: loadUserProfile,
+    validation2faEmail: send2faEmail
     // dashboard: loadDashboardStats, 
 };
 
