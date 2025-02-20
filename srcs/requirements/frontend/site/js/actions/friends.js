3a7a53941c7b41b42273188c994e484d2eccd39a
Sami Helal <shelal@bess-f3r1s15.clusters.42paris.fr>
2025-02-20 13:33:11 +0100

    shelal

--- srcs/requirements/frontend/site/js/actions/friends.js
+++ srcs/requirements/frontend/site/js/actions/friends.js
@@ -1,4 +1,6 @@
 import { navigate  } from "../router.js"
+import { authFetchJson } from "../api.js";
+
 
 export const friendsActions = [
     {
@@ -15,14 +17,6 @@ function initFriends()
     }, 50)
 }
 
-const userData = 
-[
-    { "id": 1, "name": "Alice", "avatar": "https://cdn.intra.42.fr/users/25e67b6dbe3c884da9917b16321f1574/shelal.jpg", "friend": 1, "status": "online 🟢"},
-    { "id": 2, "name": "Bob", "avatar": "https://cdn.intra.42.fr/users/fe62dcf2a389dbfa5070cc1c2fe0bd93/lpaquatt.jpg" , "friend": 1, "status": "offline 🔴"},
-    { "id": 3, "name": "Charlie", "avatar": "https://cdn.intra.42.fr/users/4e1c549d8f89770ae23812a78d09a5e6/tlam.jpg", "friend": 1, "status": "online 🔴"},
-    { "id": 4, "name": "Charlie", "avatar": "https://cdn.intra.42.fr/users/1f066718b7876b71d24f1624e61f66de/jeada-si.jpg", "friend": 1, "status": "online 🟢"},
-]
-
 function appendUser(user, userlist)
 {
     const userCard = document.createElement("div");
@@ -30,8 +24,8 @@ function appendUser(user, userlist)
 
     userCard.innerHTML = `
         <div class="card user-card text-center p-3">
-            <img src="${user.avatar}" alt="Avatar de ${user.name}" class="user-avatar mx-auto" width="100" height="100>
-            <h5 class="mt-2">${user.name}</h5>
+            <img src="${user.avatar}" alt="Avatar de ${user.username}" class="user-avatar mx-auto" width="100" height="100>
+            <h5 class="mt-2">${user.username}</h5>
             <button class="btn btn-primary add-friend" data-id="${user.id}">ADD</button>
             <button class="btn btn-primary view-profile" data-action="userModal" data-id="${user.id}">Profile</button>
         </div>
@@ -39,48 +33,110 @@ function appendUser(user, userlist)
     userlist.appendChild(userCard);
 }
 
-function handleFriends()
+
+async function getFriendInfo()
 {
+    let tab = [];
+    let nbr = 1;
+    // try
+    // {
+    //     const user = await authFetchJson(`api/userprofile/display-other-profile?user_id=${nbr}`, {method: 'GET'});
+    //     console.log(user);
+    // }
+    // catch (error)
+    // {
+    //     console.log("Error.", error);
+    // }
+    while (1)
+    {
+        try
+        {
+            const user = await authFetchJson(`api/userprofile/display-other-profile?user_id=${nbr}`, {method: 'GET'});
+            console.log(user);
+            tab.push(user);
+            nbr++;
+        }
+        catch(error)
+        {
+            console.log("Error.", error);
+            break ;
+        }
+    }
+    return (tab);
+}
+
+async function getFriendStatistic(id)
+{
+    let statUser;
+    try
+    {
+        const response = await fetch(`api/dashboards/user-statistics/?user_id=${id}`); 
+        if (!response.ok)
+            throw new Error('ERROR');
+        statUser = await response.json();
+        console.log(statUser);
+    }
+    catch(error)
+    {
+        console.log("Error.")
+    }
+    return (statUser);
+}
+
+async function handleFriends()
+{
+    let userData = [];
+
     const userlist = document.getElementById('user-container');
-
+    
     const profileModal = new bootstrap.Modal(document.getElementById('profileModal'));
-
+    
     userlist.innerHTML = `
     <div class="container mt-5">
-            <button data-action="Users" class="btn btn-primary see-friend">USERS</button>
+    <button data-action="Users" class="btn btn-primary see-friend">USERS</button>
     </div>  
     `
+    userData = await getFriendInfo();
     userData.forEach(user => {
-        if(user.friend == 1)
+        if (user.is_active == true) // est ce que cest ca est connecte ?
             appendUser(user, userlist);
     })
 
-    document.querySelectorAll(".add-friend").forEach(button => {
-        button.addEventListener("click", (e) => {
-            e.target.innerText = "Remove";
-            e.target.classList.remove("btn-primary");
-            e.target.classList.add("btn-success");
-            e.target.disabled = true; // desactive lutilisation du boutton 
-        });
-    });
-    
     document.querySelectorAll(".view-profile").forEach(button => {
-        button.addEventListener("click", (e) => {
+        button.addEventListener("click", async (e) => {
+
             const userId = parseInt(e.target.getAttribute("data-id"));
-    
+
+            // const statUser = await getFriendStatistic(userId);
+
+            console.log(userId);
+            
             const user = userData.find(u => u.id === userId);
     
             document.getElementById("profile-avatar").src = user.avatar;
-            document.getElementById("profile-name").innerText = user.name;
-            // document.getElementById("profile-email").innerText = `Email: ${user.email}`;
-            // document.getElementById("profile-bio").innerText = user.bio || "Pas de bio disponible.";
-            document.getElementById("profile-status").innerText = user.status;
+            document.getElementById("profile-name").innerText = user.username;
+            document.getElementById("profile-email").innerText = `Email: ${user.email}`;
+            if (user.is_active == true)
+                document.getElementById("profile-status").innerText = "Online";
+            else
+                document.getElementById("profile-status").innerText = "Offline";
     
             profileModal.show();
         });
     });
+
+    // document.querySelectorAll(".add-friend").forEach(button => {
+    //     button.addEventListener("click", (e) => {
+    //         e.target.innerText = "Remove";
+    //         e.target.classList.remove("btn-primary");
+    //         e.target.classList.add("btn-success");
+    //         e.target.disabled = true; // desactive lutilisation du boutton 
+    //     });
+    // });
 }
 
 /*
-    - TODO --> REMPLACER PAR APPEL API PAR LA SUITE
+- TODO -->
+    - changer userData quand le pull sera fait -> recup tous les users same time 
+    - 
 */
