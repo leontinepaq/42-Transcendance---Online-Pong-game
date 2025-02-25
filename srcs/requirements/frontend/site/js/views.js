7a83ec22ab6aef2b512daac04f7e488a6121da1b
leontinepaq <leontine.paquatte@gmail.com>
2025-02-25 19:01:49 +0100

    dashboard layout ok

--- srcs/requirements/frontend/site/js/views.js
+++ srcs/requirements/frontend/site/js/views.js
@@ -15,7 +15,7 @@ async function loadView(view) {
 		app.querySelector('.container').innerHTML = html;
 		app.classList.add('active');
 		onRouteLoad[view]?.();
-		if (view == 'home' || view == 'login' || view == 'signup' || view == 'validation2faEmail')
+		if (view == 'home' || view == 'login' || view == 'signup' || view == 'dashboard')
 			PlanetAnimation.init();
 		else 
 			PlanetAnimation.exit();
