b16cdb95631e12e9f684414c181b176c12faebb4
Sami Helal <shelal@paul-f3Ar8s3.clusters.42paris.fr>
2025-03-03 11:49:07 +0100

    shelal

--- srcs/requirements/frontend/site/js/actions/pong.js
+++ srcs/requirements/frontend/site/js/actions/pong.js
@@ -3,32 +3,62 @@ import { navigate  } from "../router.js"
 export const pongActions = [
     {
         selector: '[data-action="playGameSolo"]',
-        handler: initGame
+        handler: initGameSolo
     },
     {
         selector: '[data-action="playGameMultiplayer"]',
-        handler: initGame
+        handler: initGameMulti
     },
     {
         selector: '[data-action="playGameOnline"]',
-        handler: initGame
+        handler: initGameOnline
     },
 ];
 
-function initGame()
+let socket;
+
+function initGameSolo()
 {
     navigate('pong');
     setTimeout(function() {
+        socket = new WebSocket("/ws/pong/solo/");   // solo
         playGame();
-    }, 500)
+    }, 150)
 }
 
-export function playGame()
+function initGameMulti()
 {
-    const socket = new WebSocket("/ws/pong/multi/");   // solo - multi - online/id
-    const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
+    navigate('pong');
+    setTimeout(function() {
+        socket = new WebSocket("/ws/pong/multi/");   // multi
+        playGame();
+    }, 150)
+}
 
+function initGameOnline()
+{
+    navigate('pong');
+    setTimeout(function() {
+        socket = new WebSocket("/ws/pong/online/");   // online/id --> comment specifier lid en question ? voir avec ja
+        playGame();
+    }, 150)
+}
+
+function drawBall(x, y, r, ctx, canvas)
+{
+    ctx.beginPath();
+    ctx.arc(
+        x * canvas.width / 100,  // X coordinate of the ball
+        y * canvas.height / 100, // Y coordinate of the ball
+        r * canvas.width / 100,  // radius of the ball
+        0,                       // Starting angle
+        Math.PI * 2              // Ending angle -> 360 for complete circle
+    );
+    ctx.fill();
+};
+
+function handleSocket()
+{
     socket.onopen = function ()
     {
         console.log("✅ WebSocket connected!");
@@ -43,20 +73,22 @@ export function playGame()
     {
         console.log("🔴 WebSocket closed.");
     };
+}
 
-    function drawBall(x, y, r)
+export function closeSocket()
+{
+    if (socket)
     {
-        ctx.beginPath();
-        ctx.arc(
-            x * canvas.width / 100,  // X coordinate of the ball
-            y * canvas.height / 100, // Y coordinate of the ball
-            r * canvas.width / 100,  // radius of the ball
-            0,                       // Starting angle
-            Math.PI * 2              // Ending angle -> 360 for complete circle
-        );
-        ctx.fill();
-    };
+        socket.close();
+    }
+}
 
+function playGame()
+{
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
+
+    handleSocket();
     socket.onmessage = function(event)
     {
         console.log("Received: ", event);
@@ -77,7 +109,7 @@ export function playGame()
         ctx.fillStyle = "white";
     
         // Draw ball
-        drawBall(state.ball.x, state.ball.y, state.ball.r);
+        drawBall(state.ball.x, state.ball.y, state.ball.r, ctx, canvas);
         // Draw paddles
         ctx.fillRect(
             state.left.top_left_corner.x * canvas.width / 100,  // X coordinate of top left corner of the rectangle
@@ -134,116 +166,120 @@ export function playGame()
             }));
         }
     });
-    // let baseWidth = 480;
-    // let baseHeight = 320;
-    
-    // let leftUp, leftDown, rightUp, rightDown;
 
-    // const pong = document.getElementById('Pong');
-    // // todo: revoir la taille dinitialisaton du canvas 
-    // pong.innerHTML =
-    // `
-    //     <canvas id="myCanvas" width=980 height=680"></canvas>
-    // `;
-    
-    // let canvas = document.getElementById('myCanvas');
-    // let ctx = canvas.getContext("2d");
-
-    // function resize()
-    // {
-    //     let canvasWidth = window.innerHeight;
-    //     let canvasHeight = canvasWidth * (baseHeight / baseWidth);
-
-    //     canvas.height = canvasHeight;
-    //     canvas.width = canvasWidth;
-    //     drawFont();
-    //     drawBall();
-    //     drawPaddleLeft();
-    //     drawPaddleRight();
-    // }
-
-    // function keyDownHandler(e)
-    // {
-    //     if (e.key == "ArrowDown")
-    //         rightDown = true;
-    //     else if (e.key == "ArrowUp")
-    //         rightUp = true;
-    //     if (e.key == "s" || e.key == "S")
-    //         leftDown = true;
-    //     else if (e.key == "w" || e.key == "W")
-    //         leftUp = true;
-    // }
-    
-    // function keyUpHandler(e)
-    // {
-    //     if (e.key == "ArrowDown")
-    //         rightDown = false;
-    //     else if (e.key == "ArrowUp")
-    //         rightUp = false;
-    //     if (e.key == "s" || e.key == "S")
-    //         leftDown = false;
-    //     else if (e.key == "w" || e.key == "W")
-    //         leftUp = false;
-    // }
-    
-    // function drawFont()
-    // {
-    //     ctx.beginPath();
-    //     ctx.rect(0, 0, canvas.width, canvas.height);
-    //     ctx.fillStyle = "#052f4d";
-    //     ctx.fill();
-    //     ctx.closePath();
-    // }
-    
-    // function drawBall()
-    // {
-    //     ctx.beginPath();
-    //     ctx.arc(canvas.width / 2, canvas.height / 2, 5, 0, Math.PI * 2, false);
-    //     ctx.fillStyle = "#ebedee";
-    //     ctx.fill();
-    //     ctx.closePath();
-    // }
-    
-    // function drawPaddleLeft()
-    // {
-    //     ctx.beginPath();
-    //     ctx.rect(10, (canvas.height / 2 - 40), 10, 80);
-    //     ctx.fillStyle = "rgba(9, 9, 46, 0.5)";
-    //     ctx.fill();
-    //     ctx.closePath();    
-    // }
-    
-    // function drawPaddleRight()
-    // {
-    //     ctx.beginPath();
-    //     ctx.rect(canvas.width - 20, canvas.height / 2 - 40, 10, 80);
-    //     ctx.fillStyle = "rgba(9, 9, 46, 0.5)";
-    //     ctx.fill();
-    //     ctx.closePath(); 
-    // }
-    
-    // function draw()
-    // {
-    //     drawFont();
-    //     drawBall();
-    //     drawPaddleLeft();
-    //     drawPaddleRight();
-    // }
-    
-    // window.addEventListener('resize', function() {
-    //     resize();
-    // });
-    // document.addEventListener("keydown", keyDownHandler, false);
-    // document.addEventListener("keyup", keyUpHandler, false);
-
-    // let interval = setInterval(draw, 10);
 }
+
+export default closeSocket;
+
+// let baseWidth = 480;
+// let baseHeight = 320;
+
+// let leftUp, leftDown, rightUp, rightDown;
+
+// const pong = document.getElementById('Pong');
+// // todo: revoir la taille dinitialisaton du canvas 
+// pong.innerHTML =
+// `
+//     <canvas id="myCanvas" width=980 height=680"></canvas>
+// `;
+
+// let canvas = document.getElementById('myCanvas');
+// let ctx = canvas.getContext("2d");
+
+// function resize()
+// {
+//     let canvasWidth = window.innerHeight;
+//     let canvasHeight = canvasWidth * (baseHeight / baseWidth);
+
+//     canvas.height = canvasHeight;
+//     canvas.width = canvasWidth;
+//     drawFont();
+//     drawBall();
+//     drawPaddleLeft();
+//     drawPaddleRight();
+// }
+
+// function keyDownHandler(e)
+// {
+//     if (e.key == "ArrowDown")
+//         rightDown = true;
+//     else if (e.key == "ArrowUp")
+//         rightUp = true;
+//     if (e.key == "s" || e.key == "S")
+//         leftDown = true;
+//     else if (e.key == "w" || e.key == "W")
+//         leftUp = true;
+// }
+
+// function keyUpHandler(e)
+// {
+//     if (e.key == "ArrowDown")
+//         rightDown = false;
+//     else if (e.key == "ArrowUp")
+//         rightUp = false;
+//     if (e.key == "s" || e.key == "S")
+//         leftDown = false;
+//     else if (e.key == "w" || e.key == "W")
+//         leftUp = false;
+// }
+
+// function drawFont()
+// {
+//     ctx.beginPath();
+//     ctx.rect(0, 0, canvas.width, canvas.height);
+//     ctx.fillStyle = "#052f4d";
+//     ctx.fill();
+//     ctx.closePath();
+// }
+
+// function drawBall()
+// {
+//     ctx.beginPath();
+//     ctx.arc(canvas.width / 2, canvas.height / 2, 5, 0, Math.PI * 2, false);
+//     ctx.fillStyle = "#ebedee";
+//     ctx.fill();
+//     ctx.closePath();
+// }
+
+// function drawPaddleLeft()
+// {
+//     ctx.beginPath();
+//     ctx.rect(10, (canvas.height / 2 - 40), 10, 80);
+//     ctx.fillStyle = "rgba(9, 9, 46, 0.5)";
+//     ctx.fill();
+//     ctx.closePath();    
+// }
+
+// function drawPaddleRight()
+// {
+//     ctx.beginPath();
+//     ctx.rect(canvas.width - 20, canvas.height / 2 - 40, 10, 80);
+//     ctx.fillStyle = "rgba(9, 9, 46, 0.5)";
+//     ctx.fill();
+//     ctx.closePath(); 
+// }
+
+// function draw()
+// {
+//     drawFont();
+//     drawBall();
+//     drawPaddleLeft();
+//     drawPaddleRight();
+// }
+
+// window.addEventListener('resize', function() {
+//     resize();
+// });
+// document.addEventListener("keydown", keyDownHandler, false);
+// document.addEventListener("keyup", keyUpHandler, false);
+
+// let interval = setInterval(draw, 10);
 
 // function send_RightPaddle()
 // {
-//     if (rightUp)
-//     {
-//         socket.send(JSON.stringify({side: "1", direction: "0"}));
+    //     if (rightUp)
+    //     {
+    //         socket.send(JSON.stringify({side: "1", direction: "0"}));
 //     }
 //     if (rightDown)
 //     {
