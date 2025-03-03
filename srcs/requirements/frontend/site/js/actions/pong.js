d42aa6f7f34c8b6796bcb9ed33d3c5e5be9dbb43
Sami Helal <shelal@paul-f3Ar8s3.clusters.42paris.fr>
2025-03-03 16:27:58 +0100

    shelal

--- srcs/requirements/frontend/site/js/actions/pong.js
+++ srcs/requirements/frontend/site/js/actions/pong.js
@@ -5,14 +5,14 @@ export const pongActions = [
         selector: '[data-action="playGameSolo"]',
         handler: initGameSolo
     },
-    {
-        selector: '[data-action="playGameMultiplayer"]',
-        handler: initGameMulti
-    },
-    {
-        selector: '[data-action="playGameOnline"]',
-        handler: initGameOnline
-    },
+    // {
+    //     selector: '[data-action="playGameMultiplayer"]',
+    //     handler: initGameMulti
+    // },
+    // {
+    //     selector: '[data-action="playGameOnline"]',
+    //     handler: initGameOnline
+    // },
 ];
 
 let socket;
@@ -24,27 +24,27 @@ function initGameSolo()
     navigate('pong');
     setTimeout(function() {
         socket = new WebSocket("/ws/pong/solo/");   // solo
-        playGame();
+        playGameSolo();
     }, 150)
 }
 
-function initGameMulti()
-{
-    navigate('pong');
-    setTimeout(function() {
-        socket = new WebSocket("/ws/pong/multi/");   // multi
-        playGame();
-    }, 150)
-}
+// function initGameMulti()
+// {
+//     navigate('pong');
+//     setTimeout(function() {
+//         socket = new WebSocket("/ws/pong/multi/");   // multi
+//         playGame();
+//     }, 150)
+// }
 
-function initGameOnline()
-{
-    navigate('pong');
-    setTimeout(function() {
-        socket = new WebSocket("/ws/pong/online/");   // online/id --> comment specifier lid en question ? voir avec ja
-        playGame();
-    }, 150)
-}
+// function initGameOnline()
+// {
+//     navigate('pong');
+//     setTimeout(function() {
+//         socket = new WebSocket("/ws/pong/online/");   // online/id --> comment specifier lid en question ? voir avec ja
+//         playGame();
+//     }, 150)
+// }
 
 function handleSocket()
 {
@@ -81,16 +81,12 @@ function statePause()
     ctx.font = "50px Arial";
     ctx.textAlign = "center";
 
-    // Draw two vertical bars for the pause sign
-    const barWidth = 20; // Width of the pause bars
-    const barHeight = 100; // Height of the pause bars
-    const pauseX = canvas.width / 2 - 40; // X position for the first bar
-    const pauseY = canvas.height / 2 - barHeight / 2; // Y position for the bars
+    const barWidth = 20;
+    const barHeight = 100;
+    const pauseX = canvas.width / 2 - 40;
+    const pauseY = canvas.height / 2 - barHeight / 2;
 
-    // Left bar
     ctx.fillRect(pauseX, pauseY, barWidth, barHeight);
-    
-    // Right bar
     ctx.fillRect(pauseX + 40, pauseY, barWidth, barHeight);
 }
 
@@ -98,11 +94,11 @@ function drawBall(x, y, r)
 {
     ctx.beginPath();
     ctx.arc(
-        x * canvas.width / 100,  // X coordinate of the ball
-        y * canvas.height / 100, // Y coordinate of the ball
-        r * canvas.width / 100,  // radius of the ball
-        0,                       // Starting angle
-        Math.PI * 2              // Ending angle -> 360 for complete circle
+        x * canvas.width / 100,
+        y * canvas.height / 100,
+        r * canvas.width / 100,
+        0,
+        Math.PI * 2 
     );
     ctx.fill();
 };
@@ -110,26 +106,26 @@ function drawBall(x, y, r)
 function drawPaddle(state)
 {
     ctx.fillRect(
-        state.left.top_left_corner.x * canvas.width / 100,  // X coordinate of top left corner of the rectangle
-        state.left.top_left_corner.y * canvas.height / 100, // Y coordinate of top left corner of the rectangle
-        state.left.width * canvas.width / 100,              // Width of the rectangle
-        state.left.height * canvas.height / 100             // Length of the rectangle
+        state.left.top_left_corner.x * canvas.width / 100,
+        state.left.top_left_corner.y * canvas.height / 100,
+        state.left.width * canvas.width / 100,
+        state.left.height * canvas.height / 100
     );
     ctx.fillRect(
-        state.right.top_left_corner.x * canvas.width / 100,  // X coordinate of top left corner of the rectangle
-        state.right.top_left_corner.y * canvas.height / 100, // Y coordinate of top left corner of the rectangle
-        state.right.width * canvas.width / 100,              // Width of the rectangle
-        state.right.height * canvas.height / 100             // Length of the rectangle
+        state.right.top_left_corner.x * canvas.width / 100,
+        state.right.top_left_corner.y * canvas.height / 100,
+        state.right.width * canvas.width / 100,
+        state.right.height * canvas.height / 100
     );
 }
 
-function messageSocket()
+function messageSocket() 
 {
     socket.onmessage = function(event)
     {
         console.log("Received: ", event);
         const state = JSON.parse(event.data);
-        console.log(state);
+        console.log(state.paused);
     
         if (state.alert)
         {
@@ -149,157 +145,25 @@ function messageSocket()
         {
             statePause();
         }
-    };
+    }; 
 }
 
-// function playGame()
-// {
-//     canvas = document.getElementById("gameCanvas");
-//     ctx = canvas.getContext("2d");
-    
-//     handleSocket();
-//     messageSocket();
-    // let isPaused = false;
-
-    // document.addEventListener("keydown", (event) => {
-    //     const keyActions = {
-    //         "ArrowUp": { side: "right", paddle: -2 },
-    //         "ArrowDown": { side: "right", paddle: 2 },
-    //         "q": { side: "left", paddle: -2 },
-    //         "a": { side: "left", paddle: 2 },
-    //         " ": { toggle_pause: true}
-    //     };
-
-    //     if (keyActions[event.key])
-    //     {
-    //         event.preventDefault();
-    //         if (event.key === " " && !isPaused)
-    //         {
-    //             isPaused = true;
-    //             socket.send(JSON.stringify({
-    //                 toggle_pause: true,
-    //                 side: keyActions[event.key].side || "left",
-    //                 paddle: keyActions[event.key].paddle || 0
-    //             }));    
-    //         }
-    //         else if (event.key === " " && isPaused)
-    //         { 
-    //             isPaused = false;
-    //             socket.send(JSON.stringify({
-    //                 toggle_pause: false,
-    //                 side: keyActions[event.key].side || "left",
-    //                 paddle: keyActions[event.key].paddle || 0
-    //             }));
-    //         }
-    //         else
-    //         {
-    //             socket.send(JSON.stringify({
-    //                 toggle_pause: false,
-    //                 side: keyActions[event.key].side || "left",
-    //                 paddle: keyActions[event.key].paddle || 0
-    //             }));
-    //         }
-    //     }
-    // });
-// }
-
-export default closeSocket;
-
-// function messageSocket()
-// {
-//     socket.onmessage = function(event)
-//     {
-//         console.log("Received: ", event);
-//         const state = JSON.parse(event.data);
-//         console.log(state);
-    
-//         if (state.alert)
-//         {
-//             alert(state.message);
-//         };
-        
-//         //Score
-//         document.getElementById("leftScore").textContent = state.score[0];
-//         document.getElementById("rightScore").textContent = state.score[1];
-    
-//         // Clear canvas
-//         ctx.clearRect(0, 0, canvas.width, canvas.height); 
-//         ctx.fillStyle = "white";
-    
-//         // Draw ball
-//         drawBall(state.ball.x, state.ball.y, state.ball.r, ctx, canvas);
-//         // Draw paddles
-//         ctx.fillRect(
-//             state.left.top_left_corner.x * canvas.width / 100,  // X coordinate of top left corner of the rectangle
-//             state.left.top_left_corner.y * canvas.height / 100, // Y coordinate of top left corner of the rectangle
-//             state.left.width * canvas.width / 100,              // Width of the rectangle
-//             state.left.height * canvas.height / 100             // Length of the rectangle
-//         );
-//         ctx.fillRect(
-//             state.right.top_left_corner.x * canvas.width / 100,  // X coordinate of top left corner of the rectangle
-//             state.right.top_left_corner.y * canvas.height / 100, // Y coordinate of top left corner of the rectangle
-//             state.right.width * canvas.width / 100,              // Width of the rectangle
-//             state.right.height * canvas.height / 100             // Length of the rectangle
-//         );
-    
-//         // Draw PAUSED text if the game is paused
-//         if (state.paused) {
-//             ctx.fillStyle = "rgba(255, 255, 255, 0.7)"; // Light overlay
-//             ctx.fillRect(0, 0, canvas.width, canvas.height); // Transparent overlay
-    
-//             ctx.fillStyle = "white";
-//             ctx.font = "50px Arial";
-//             ctx.textAlign = "center";
-    
-//             // Draw two vertical bars for the pause sign
-//             const barWidth = 20; // Width of the pause bars
-//             const barHeight = 100; // Height of the pause bars
-//             const pauseX = canvas.width / 2 - 40; // X position for the first bar
-//             const pauseY = canvas.height / 2 - barHeight / 2; // Y position for the bars
-    
-//             // Left bar
-//             ctx.fillRect(pauseX, pauseY, barWidth, barHeight);
-            
-//             // Right bar
-//             ctx.fillRect(pauseX + 40, pauseY, barWidth, barHeight);
-//         }
-//     };
-// }
-
-/*
-    TODO :
-        - changer la vitesse de deplacement des paddles
-*/
-
-let pressedKeys = {
-    "ArrowUp": false,
-    "ArrowDown": false,
-    "q": false,
-    "a": false,
-    " ": false
-}
-
-function playGame()
+function playGameSolo()
 {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
     
     handleSocket();
     messageSocket();
-    let isPaused = false;
+    let isPaused = true;
 
     document.addEventListener("keydown", (event) => {
-        if (pressedKeys.hasOwnProperty(event.key)) {
-            pressedKeys[event.key] = false;
-        }
         const keyActions = {
-            // "ArrowUp": { side: "right", paddle: -2 },
-            // "ArrowDown": { side: "right", paddle: 2 },
-            // "q": { side: "left", paddle: -2 },
-            // "a": { side: "left", paddle: 2 },
-            " ": { toggle_pause: true}
+            "ArrowUp": { side: "right", paddle: -2 },
+            "ArrowDown": { side: "right", paddle: 2 },
+            " ": { toggle_pause: false}
         };
-
+        
         if (keyActions[event.key])
         {
             event.preventDefault();
@@ -309,69 +173,33 @@ function playGame()
                 socket.send(JSON.stringify({
                     toggle_pause: true,
                     side: keyActions[event.key].side || "left",
-                    paddle: keyActions[event.key].paddle || 0 
+                    paddle: keyActions[event.key].paddle || 0
                 }));    
             }
             else if (event.key === " " && isPaused)
             { 
                 isPaused = false;
+                socket.send(JSON.stringify({ 
+                    toggle_pause: false,
+                    side: keyActions[event.key].side || "left",
+                    paddle: keyActions[event.key].paddle || 0
+                }));
+            }
+            else
+            {
                 socket.send(JSON.stringify({
                     toggle_pause: false,
                     side: keyActions[event.key].side || "left",
                     paddle: keyActions[event.key].paddle || 0
                 }));
             }
-            // else
-            // {
-            //     socket.send(JSON.stringify({
-            //         toggle_pause: false,
-            //         side: keyActions[event.key].side || "left", 
-            //         paddle: keyActions[event.key].paddle || 0
-            //     }));
-            // }
-        }
-        handlePaddleMovement();
-    });
-
-    document.addEventListener("keyup", (event) => {
-        if (pressedKeys.hasOwnProperty(event.key)) {
-            pressedKeys[event.key] = true;
         }
     });
 }
 
-function handlePaddleMovement()
-{
-    if (pressedKeys["ArrowUp"])
-    {
-        socket.send(JSON.stringify({
-            toggle_pause: false,
-            side: "right",
-            paddle: 2
-        }));
-    }
-    if (pressedKeys["ArrowDown"])
-    {
-        socket.send(JSON.stringify({
-            toggle_pause: false,
-            side: "right",
-            paddle: -2
-        }));
-    }
-    if (pressedKeys["w"]) 
-    {
-        socket.send(JSON.stringify({
-            toggle_pause: false,
-            side: "left",
-            paddle: 2
-        }));
-    }
-    if (pressedKeys["s"])
-    {
-        socket.send(JSON.stringify({
-            toggle_pause: false,
-            side: "left",
-            paddle: -2
-        }));
-    }
-}
+/*
+    - est ce que tout est reset quand la connexion websocket se ferme ?
+*/
+
+export default closeSocket;
+
