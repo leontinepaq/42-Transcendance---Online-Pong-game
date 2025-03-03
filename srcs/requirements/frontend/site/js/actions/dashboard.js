36fd43c58b1bd948556a3318abf4a8778a017561
leontinepaq <leontine.paquatte@gmail.com>
2025-03-03 15:47:19 +0100

    avance sur dashboard

--- srcs/requirements/frontend/site/js/actions/dashboard.js
+++ srcs/requirements/frontend/site/js/actions/dashboard.js
@@ -1,60 +1,63 @@
-import { authFetchJson, handleError }	from "../api.js";
-
+import { authFetchJson, handleError } from "../api.js";
 
 async function updateStatValues(data) {
-	//todo @leontinepaq a changer quand fonctionne
-	data.wins = 54;
-	data.winstreak = 1;
-	data.total_time_played = "0:25:12";
-	data.unique_opponents_count = 2;
-	// //
+  //todo @leontinepaq a changer quand fonctionne
+  data.wins = 60;
+  data.winstreak = 1;
+  data.total_time_played = "0:25:12";
+  data.unique_opponents_count = 2;
+  // //
 
-	document.getElementById('matches-won').textContent = data.wins;
-	document.getElementById('win-streak').textContent = data.winstreak;
-	document.getElementById('total-time').textContent = data.total_time_played;
-	document.getElementById('online-opponents').textContent = data.unique_opponents_count;
+  document.getElementById("matches-won").textContent = data.wins;
+  document.getElementById("win-streak").textContent = data.winstreak;
+  document.getElementById("total-time").textContent = data.total_time_played;
+  document.getElementById("online-opponents").textContent =
+    data.unique_opponents_count;
 }
 
-async function plotWinRate(data)
-{
-	const ctx = document.getElementById("win-rate-chart").getContext("2d");
+async function plotWinRate(data) {
+  const ctx = document.getElementById("win-rate-chart").getContext("2d");
 
-	const winColor = getComputedStyle(document.documentElement).getPropertyValue("--color-accent").trim();
-	const lossColor = getComputedStyle(document.documentElement).getPropertyValue("--color-accent-light").trim();
+  const winColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-accent");
+  const lossColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-accent-light");
 
-	data.winRate = 60; //todo @leontinepaq a changer quand fonctionne
-	
-	new Chart(ctx, {
-		type: "doughnut",
-		data: {
-			labels: ["winning", "losing"],
-			datasets: [{
-				data: [data.winRate, 100 - data.winRate],
-				backgroundColor: [winColor, lossColor],
-				borderWidth: 0
-			}]
-		},
-		options: {
-			maintainAspectRatio: true,
-			cutout: "50%",
-			plugins: {
-				legend: { display: false}
-			}
-		},
-	});
-	document.getElementById('win-rate-percentage').textContent = `${data.winRate}%`;
+  data.winRate = 60; //todo @leontinepaq a changer quand fonctionne
+
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: ["winning", "losing"],
+      datasets: [
+        {
+          data: [data.winRate, 100 - data.winRate],
+          backgroundColor: [winColor, lossColor],
+          borderWidth: 0,
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: true,
+      cutout: "50%",
+      plugins: {
+        legend: { display: false },
+      },
+    },
+  });
+  document.getElementById(
+    "win-rate-percentage"
+  ).textContent = `${data.winRate}%`;
 }
 
-export async function loadUserStats()
-{
-	try {
-		const data = await authFetchJson('api/dashboard/dis play-user-stats/', {method: 'GET'});
-		console.log(data);
-		updateStatValues(data);
-		plotWinRate(data);
-	}
-	catch (error) {
-		handleError(error, "Load user stats error");
-	}	
+export async function loadUserStats() {
+  try {
+    const data = await authFetchJson("api/dashboard/display-user-stats/", {
+      method: "GET",
+    });
+    updateStatValues(data);
+    plotWinRate(data);
+  } catch (error) {
+    handleError(error, "Load user stats error");
+  }
 }
-
