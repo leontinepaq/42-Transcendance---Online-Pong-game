449a76460dd02815215ec5db9350a09bbac55da5
Jean-antoine Da silva <jeada-si@made-f0Ar1s7.clusters.42paris.fr>
2025-04-23 11:48:12 +0200

    populate_db fi

--- srcs/requirements/backend/populate_db.py
+++ srcs/requirements/backend/populate_db.py
@@ -15,25 +15,26 @@ Participant.objects.all().delete()
 Tournament.objects.all().delete()
 
 admin = UserModel.objects.create_superuser("admin", "admin@admin.fr", "admin")
-admin.avatar_url="/media/avatars/admin.jpg"
-admin.is_active=False
+admin.avatar_url = "/media/avatars/admin.jpg"
+admin.is_active = False
 admin.save()
 
 for name in usernames:
     if not UserModel.objects.filter(username=name).exists():
-        user=UserModel.objects.create_user(name, name + "@test.com", password='test')
-        user.avatar_url=f"/media/avatars/{name}.jpg"
+        user = UserModel.objects.create_user(
+            name, name + "@test.com", password='test')
+        user.avatar_url = f"/media/avatars/{name}.jpg"
         user.save()
         ids.append(user.id)
         users.append(user)
 
 
-for i in range(0,50):
+for i in range(0, 50):
     score_1 = random.randint(0, 2)
     score_2 = random.randint(0, 2)
     longest_exchange = random.randint(0, 100)
     duration = datetime.timedelta(minutes=random.randint(0, 5),
-                                       seconds=random.randint(0, 60))
+                                  seconds=random.randint(0, 60))
     if random.randint(0, 1):
         score_1 = 3
     else:
@@ -54,33 +55,31 @@ for i in range(0,50):
                                               longest_exchange)
     game.created_at = timezone.now() - datetime.timedelta(days=random.randint(0, 10))
     game.save()
-    
+
 for i in range(0, 30):
     players = random.sample(ids, random.randint(2, 4))
     tournament = Tournament.create_tournament(f"Tournament {i}", players[0])
     for player in players[1:]:
         tournament.players.add(Participant.get(player))
         tournament.save()
-        
-        
+
+
 for i in range(0, 4):
     tournaments = Tournament.objects.filter(finished=False)
     for tournament in tournaments:
-        if tournament.get_next_match() == None:
-            continue
-        if random.randint(0, 10) > 8:
-            continue
-        score_1 = random.randint(0, 2)
-        score_2 = random.randint(0, 2)
-        longest_exchange = random.randint(0, 100)
-        duration = datetime.timedelta(minutes=random.randint(0, 5),
-                                        seconds=random.randint(0, 60))
-        player1, player2 = tournament.get_next_match()
-        game, tournament_winner = Game.create(player1.id,
-                                              player2.id,
-                                              score_1,
-                                              score_2,
-                                              duration,
-                                              longest_exchange,
-                                              tournament.id)
-    
+        next = tournament.get_next_match()
+        for match in next:
+            if match.id:
+                continue
+            score_1 = random.randint(0, 3)
+            score_2 = random.randint(0, 2) if score_1 == 3 else 3
+            longest_exchange = random.randint(0, 100)
+            duration = datetime.timedelta(minutes=random.randint(0, 5),
+                                          seconds=random.randint(0, 60))
+            game, tournament_winner = Game.create(match.player1.user.id,
+                                                  match.player2.user.id,
+                                                  score_1,
+                                                  score_2,
+                                                  duration,
+                                                  longest_exchange,
+                                                  tournament.id)
